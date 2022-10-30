27b38dd2357bed30c923dca3a8ad3fbb0320bab8
alikhodaverdi <alikhodaveerdi@yahoo.com>
2022-10-30 11:09:47 +0330

    Retrieve , Create

--- MyTest/posts/views.py
+++ MyTest/posts/views.py
@@ -1,3 +1,4 @@
+from http.client import BAD_REQUEST
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -13,4 +14,23 @@ class PostListView(APIView):
     def get(self,request):
         posts = Post.objects.all()
         serializer = PostSerializer(posts , many=True)
+        return Response(serializer.data)
+    
+    def post(self,request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors , status=BAD_REQUEST)
+    
+
+    
+class PostDetailView(APIView):
+    def get(self,request ,post_id):
+        try: 
+            post = Post.objects.get(pk =post_id)
+        except Post.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = PostSerializer(post)
         return Response(serializer.data)
