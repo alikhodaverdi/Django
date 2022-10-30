396441cf76f4feb5baabb8e972801b147aedbb85
alikhodaverdi <alikhodaveerdi@yahoo.com>
2022-10-30 10:33:54 +0330

    Create post view & delete format urk

--- MyTest/posts/models.py
+++ MyTest/posts/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from unicodedata import category
 from unittest.util import _MAX_LENGTH
 from django.db import models
@@ -7,6 +8,11 @@ from django.db import models
 class Post(models.Model) : 
     title = models.CharField(max_length=255)
     summery = models.CharField(max_length=255)
+    
+    last_updated_on = models.DateTimeField(auto_now=True)
+    
+    is_active = models.BooleanField(default=True)
+    
     
 def __str__(self):
     return self.name
--- MyTest/posts/views.py
+++ MyTest/posts/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseRedirect,HttpResponseNotFound
-
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from yaml import serialize
 
+from .models import Post
+from .serlializers import PostSerializer
 
 # Create your views here.
 
 
-@api_view(['GET'])
-def index(request):
-    return Response("Hello mehrdadss")
+class PostListView(APIView):
+    def get(self,request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts , many=True)
+        return Response(serializer.data)
